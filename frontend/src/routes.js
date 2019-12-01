86e0c2bd9afe54770b76a0c264d9dc6d6cf221ee
Roger Souza <xrogeriosouza@gmail.com>
2019-12-01 16:55:38 -0300

    adicionando arquivos

---
+++ frontend/src/routes.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter , Route, Switch } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import New from './pages/New';
+
+export default function Routes(){
+  return (
+      <BrowserRouter >
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/new" component={New} />
+        </Switch>
+      </BrowserRouter >
+  );
+}
+
+/*import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+const Routes = () => (
+  <Router>
+      <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/new" component={New} />
+      </Switch>
+  </Router>
+);
+export default Routes;*/
